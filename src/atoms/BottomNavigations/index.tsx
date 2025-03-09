4e2f9d6def3254e0cbd31c1adbd111d647202d2b
AnggaWikaNugraha <anggawika18@gmail.com>
2025-03-09 21:48:01 +0700

    custome tab bar

--- src/atoms/BottomNavigations/index.tsx
+++ src/atoms/BottomNavigations/index.tsx
@@ -2,14 +2,33 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { HomeScreen } from "../../view/ScreenHome";
 import { ScreenParamsList } from "../../type/ScreenParamsList";
 import { DetailsScreen } from "../../view/ScreenDetails";
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 // Buat Bottom Tab Navigator
 const Tab = createBottomTabNavigator<ScreenParamsList>();
 
 export const HomeTabs = () => {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Navigator screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+
+                if (route.name === 'Home') {
+                    iconName = focused
+                        ? 'ios-information-circle'
+                        : 'ios-information-circle-outline';
+                } else if (route.name === 'Settings') {
+                    iconName = focused ? 'ios-list' : 'ios-list-outline';
+                }
+
+                // You can return any component that you like here!
+                return <Ionicons name={iconName ?? ''} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'tomato',
+            tabBarInactiveTintColor: 'gray',
+
+        })}>
+            <Tab.Screen options={{ tabBarBadge: 3 }} name="Home" component={HomeScreen} />
             <Tab.Screen name="Details" component={DetailsScreen} />
         </Tab.Navigator>
     );
